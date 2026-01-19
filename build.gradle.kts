b2deaa8a14292f1f0a9e990ed7513e17dabaabd9
curtishd <1023632660@qq.com>
2026-01-19 11:10:21 +0800

    Bump kotlinx version to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.2.20"
+    kotlin("jvm") version "2.3.0"
     application
 }
 
@@ -23,7 +23,7 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 tasks.register<Copy>("copyResourcesToClass") {
