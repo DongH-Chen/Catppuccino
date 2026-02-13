7ce8c30d7a2639e5103a41640807c78156177cb0
DongH-Chen <1023632660@qq.com>
2026-02-13 10:59:04 +0800

    build: Update build.gradle.kts file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,41 @@ java {
     }
 }
 
+tasks.register<Jar>("fatJar") {
+    group = "build"
+
+    manifest {
+        attributes(
+            "Main-Class" to "dev.cdh.Main",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Implementation-Vendor" to "me.cdh",
+            "Created-By" to "Gradle ${gradle.gradleVersion}",
+            "Built-By" to System.getProperty("user.name"),
+            "Build-Jdk" to System.getProperty("java.version")
+        )
+    }
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
+
+//    archiveClassifier.set("all")
+}
+
+//tasks.build {
+//    dependsOn("fatJar")
+//}
+
 tasks.test {
     useJUnitPlatform()
 }
